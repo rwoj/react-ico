c0c1b15de9d9d6443032cef35bd7c58f43d0c030
streamich <streamich@gmail.com>
2018-03-30 14:18:16 +0100

    feat: style <Sidebar> component

--- app/containers/Sidebar/index.js
+++ app/containers/Sidebar/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {rule} from '../../nano';
+
+const width = 300;
+const className = rule({
+    z: 100,
+    bdr: '1px solid #ccc',
+    pos: 'fixed',
+    w: width + 'px',
+    h: '100%',
+    top: 0,
+    left: 0,
+    ov: 'hidden',
+    trs: 'left 0.2s',
+    willChange: 'left',
+}, 'Sidebar');
 
 const Sidebar = () => {
-
-    return <div>sidebar</div>;
+    return <div className={className}>sidebar</div>;
 };
 
 export default Sidebar;
